9cfab98e15207b21226d335f609aa3b812624160
James Baker <jamesbaker@Jamess-MacBook-Air-331.local>
2025-12-30 14:24:34 -0500

    headache

--- donwloading.py
+++ donwloading.py
@@ -1,17 +1,25 @@
 import gdown
 import os
 
-file_id = "1RYKdbjEfeRovyfH4iiautbfl6yh2-GEs"
-output = "cuhk_sysu.zip"  # change the name as needed
+pair_list=[
+    ["1RYKdbjEfeRovyfH4iiautbfl6yh2-GEs","cuhk_sysu"],
+    ["1pKw7rzP_KuI2WDX5YxEEg1NdGQYPpjMl","prw"],
+    ["1TKIzsUUo4zlNJFLT1_KzQCsL2zkOzwUJ","mvn"]
+]
 
-if not os.path.exists(output):
-    gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
+for [file_id, directory] in pair_list:
 
-# Source - https://stackoverflow.com/a/3451150
-# Posted by Rahul, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-12-30, License - CC BY-SA 4.0
+    #file_id = "1RYKdbjEfeRovyfH4iiautbfl6yh2-GEs"
+    output =   directory+".zip"  # change the name as needed
 
-import zipfile
-directory="cuhk_sysu"
-with zipfile.ZipFile(output, 'r') as zip_ref:
-    zip_ref.extractall(directory)
+    if not os.path.exists(output):
+        gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
+
+    # Source - https://stackoverflow.com/a/3451150
+    # Posted by Rahul, modified by community. See post 'Timeline' for change history
+    # Retrieved 2025-12-30, License - CC BY-SA 4.0
+
+    import zipfile
+    directory="cuhk_sysu"
+    with zipfile.ZipFile(output, 'r') as zip_ref:
+        zip_ref.extractall(directory)
