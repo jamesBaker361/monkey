1d345f4e37043166245eec098eeafc7b1cacf8b4
James Baker <jlbaker361@gmail.com>
2026-01-02 01:05:01 -0500

    p

--- pedestrian.py
+++ pedestrian.py
@@ -226,7 +226,7 @@ def main(args):
             gen=pipe(prompt,height,width,args.initial_steps,
                            ip_adapter_image=query,generator=generator,timesteps=timesteps[args.offset:],latents=noisy_latents).images[0]
             mask=sum([get_mask_rect(args.layer_index,attn_list,step,args.token,args.threshold,latent_dim_x,latent_dim_y) for step in mask_step_list])
-            mask=F.interpolate(mask.unsqueeze(0).unsqueeze(0), size=(img_x,img_y), mode="nearest").squeeze(0).squeeze(0)
+            mask=F.interpolate(mask.unsqueeze(0).unsqueeze(0), size=(img_y,img_x), mode="nearest").squeeze(0).squeeze(0)
 
             mask_pil=to_pil_image(mask)
             draw=ImageDraw.Draw(gallery)
