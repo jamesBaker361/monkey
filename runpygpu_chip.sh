c9530914f9029c5b54d02cd63f26a5a289b4cd2c
James Baker <jlbaker361@gmail.com>
2026-09-21 18:59:55 -0400

    mem

--- runpygpu_chip.sh
+++ runpygpu_chip.sh
@@ -12,7 +12,7 @@
 
 #SBATCH --gres=gpu:1
 
-#SBATCH --mem=192000                # Real memory (RAM) required (MB)
+#SBATCH --mem=32000                # Real memory (RAM) required (MB)
 
 #SBATCH --time=3-00:00:00           # Total run time limit (D-HH:MM:SS)
 
